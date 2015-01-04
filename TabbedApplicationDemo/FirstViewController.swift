78dfbd58e99634c64f33557275cd379f2ad84bdf
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-03 19:45:31 -0800

    Initial Commit

---
+++ TabbedApplicationDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedApplicationDemo
+//
+//  Created by Yosemite on 1/3/15.
+//  Copyright (c) 2015 Yosemite. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TabbedApplicationDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedApplicationDemo
+//
+//  Created by Yosemite on 1/3/15.
+//  Copyright (c) 2015 Yosemite. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
